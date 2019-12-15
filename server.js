7bf4cd11f4b5e22a946344cd3037f8d5577380fe
Ben Tait <bentait@Bens-MBP.lan1>
2019-12-14 20:35:08 -0500

    db connection updated in server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-convex-61483',
       user : 'bentait',
       password : '',
       database : 'memryiodb'
