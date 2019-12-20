d4b2e2ff4d6b0e203e229c71cf051cf40809b0cc
Ben Tait <bentait@Bens-MBP.lan1>
2019-12-20 15:57:42 -0500

    getSignedURL

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors());
 
 
 app.get('/',(req,res) =>{
-    res.json('memriio is live : aws + dotenv')
+    res.json('memriio is live : getsignedfileURL')
 })
 
 app.post('/signin',(req,res) => {
@@ -182,7 +182,45 @@ app.post('/searchuser',(req,res) =>{
         })
     .catch(err=> res.status(400).json('error searching memories'))
 })
-
+aws.config.update({
+    region: 'us-east-1', // Put your aws region here
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  })
+  
+  const S3_BUCKET = process.env.S3_BUCKET
+  // Now lets export this function so we can call it from somewhere else
+  exports.getSignedURL = (req,res) => {
+    const s3 = new aws.S3();  // Create a new instance of S3
+    const fileName = req.body.fileName;
+    const fileType = req.body.fileType;
+      // Set up the payload of what we are sending to the S3 api
+      const s3Params = {
+          Bucket: S3_BUCKET,
+          Key: fileName,
+          Expires: 500,
+          ContentType: fileType,
+          ACL: 'public-read'
+      };
+      // Make a request to the S3 API to get a signed URL which we 
+      // can use to upload our file
+  
+      s3.getSignedUrl('putObject', s3Params, (err, data) => {
+              if(err){
+                  console.log(err);
+                  res.json({success: false, error: err})
+              }
+              // Data payload of what we are sending back, the url 
+              // of the signedRequest and a URL where we can 
+              // access the content after its saved. 
+          const returnData = {
+                  signedRequest: data,
+                  url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+                  };
+                  // Send it all back
+          res.json({success:true, data:{returnData}});
+      });
+  }
 
 app.listen(process.env.PORT || 3000,()=> {
     console.log('app running on port ${process.env.PORT}');
