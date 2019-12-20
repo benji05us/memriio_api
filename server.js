8fce716a60a40929bfb1e34cb2c8d5beee5b2f49
Ben Tait <bentait@Bens-MBP.lan1>
2019-12-20 16:34:30 -0500

    aws testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const knex = require('knex')
 const aws = require('aws-sdk')
 require('dotenv').config(); // Configure dotenv to load in the .env file
-
+const S3_BUCKET = process.env.S3_BUCKET
 const db = knex({
     client: 'pg',
     connection: {
@@ -19,8 +19,6 @@ aws.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
-  
-const S3_BUCKET = process.env.S3_BUCKET
 
 
 const app = express();
@@ -29,7 +27,7 @@ app.use(cors());
 
 
 app.get('/',(req,res) =>{
-    res.json('memriio is live : getsignedfileURL 2')
+    res.json('memriio is live : getsignedfileURL 3')
 })
 
 app.post('/signin',(req,res) => {
@@ -82,6 +80,41 @@ app.post('/register',(req,res) => {
     .then()
     
 })
+
+app.post ('/signedURL',(req,res) =>{
+
+    console.log('made it to getSignedURL', req.body);
+    const s3 = new aws.S3(); // Create a new instance of S3
+    const fileName = req.body.fileName;
+    const fileType = req.body.fileType;
+    // Set up the payload of what we are sending to the S3 api
+    const s3Params = {
+        Bucket: S3_BUCKET,
+        Key: fileName,
+        Expires: 500,
+        ContentType: fileType,
+        ACL: 'public-read'
+    };
+    // Make a request to the S3 API to get a signed URL which we 
+    // can use to upload our file
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.json({ success: false, error: err });
+        }
+        // Data payload of what we are sending back, the url 
+        // of the signedRequest and a URL where we can 
+        // access the content after its saved. 
+        const returnData = {
+            signedRequest: data,
+            url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+        };
+        // Send it all back
+        res.json({ success: true, data: { returnData } });
+    });
+
+})
+
 
 app.post('/post',(req,res) => {
     const {ispersonal,userid,groupid} = req.body
@@ -193,40 +226,6 @@ app.post('/searchuser',(req,res) =>{
 
 
 
-  export function getSignedURL(req,res)  {
-    
-    console.log('made it to getSignedURL',req.body);
-    
-    const s3 = new aws.S3();  // Create a new instance of S3
-    const fileName = req.body.fileName;
-    const fileType = req.body.fileType;
-      // Set up the payload of what we are sending to the S3 api
-      const s3Params = {
-          Bucket: S3_BUCKET,
-          Key: fileName,
-          Expires: 500,
-          ContentType: fileType,
-          ACL: 'public-read'
-      };
-      // Make a request to the S3 API to get a signed URL which we 
-      // can use to upload our file
-  
-      s3.getSignedUrl('putObject', s3Params, (err, data) => {
-              if(err){
-                  console.log(err);
-                  res.json({success: false, error: err})
-              }
-              // Data payload of what we are sending back, the url 
-              // of the signedRequest and a URL where we can 
-              // access the content after its saved. 
-          const returnData = {
-                  signedRequest: data,
-                  url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
-                  };
-                  // Send it all back
-          res.json({success:true, data:{returnData}});
-      });
-  }
 
 app.listen(process.env.PORT || 3000,()=> {
     console.log('app running on port ${process.env.PORT}');
