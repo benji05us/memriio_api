f8a74cfb8f8e3fc23dde9c5ffcea013ab6327ad5
Ben Tait <bentait@Bens-MacBook-Pro.local>
2019-12-25 19:29:49 -0500

    tempData version

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors());
 // root ----------------------
 
 app.get('/',(req,res) =>{
-    res.json('memriio is live : getsignedfileURL 3')
+    res.json('memriio is live : getsignedfileURL 4')
 })
 
 // signin ----------------------
@@ -98,7 +98,6 @@ app.post ('/signedurl',(req,res) =>{
     const s3 = new aws.S3(); // Create a new instance of S3
     const fileName = req.body.fileName;
     const fileType = req.body.fileType;
-    // Set up the payload of what we are sending to the S3 api
 
     console.log('filename :',fileName)
 
@@ -109,21 +108,16 @@ app.post ('/signedurl',(req,res) =>{
         ContentType: fileType,
         ACL: 'public-read'
     };
-    // Make a request to the S3 API to get a signed URL which we 
-    // can use to upload our file
+    
     s3.getSignedUrl('putObject', s3Params, (err, signedURL) => {
         if (err) {
             console.log('Error in s3.getSignedURL',err);
             res.json({ success: false, error: err });
         }else{
-            // Data payload of what we are sending back, the url 
-            // of the signedRequest and a URL where we can 
-            // access the content after its saved. 
-            
             const returnData = {
 
                 signedRequest: signedURL,
-                url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+                url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}.${fileType}`
             };
             console.log('signedURL : ',returnData.signedRequest);
             console.log('url       : ',returnData.url);
