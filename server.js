4909ce99ddbc0603431c749aedf557227f99bdf1
Ben Tait <bentait@Bens-MacBook-Pro.local>
2019-12-26 18:23:10 -0500

    as

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.post('/creatememory',(req,res) => {
 app.post('/addtest',(req,res) => {
 
     console.log('addtest :',req.body);
-    res.json(res.status(200).json('add test servier side'))
+    res.status(200).json('add test servier side')
     
 })
 
