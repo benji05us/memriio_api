48c05678748a58f6c3fb0b11d8c06ca59fa51f7b
Ben Tait <bentait@Bens-MacBook-Pro.local>
2019-12-27 19:51:17 -0500

    retwer

--- server.js
+++ server.js
@@ -273,7 +273,7 @@ app.post('/searchuser',(req,res) =>{
     // id,groupid,userid,herourl
     db.select('memories.id','memories.groupid','memories.userid','memfiles.fileurl')
     .join('memfiles', {'memfiles.memid': 'memories.id'})
-    console.log(db.toString())
+    console.log(db.toSQL().toNative())
     
 
     // db.select('memories.id','memories.groupid','memories.userid','memfiles.fileurl')
